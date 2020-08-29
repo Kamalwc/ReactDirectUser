109003299c768aa09cb3b9b2af68e874d82f749d
kamalwc <carmichaelkw@gmail.com>
2020-08-29 17:24:36 -0400

    Updates

---
+++ precache-manifest.0b7ee8d9ff236914b2f899a805aab250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3153fc82632e772035ab5f11483e17",
+    "url": "/ReactDirectUser/index.html"
+  },
+  {
+    "revision": "27f128e182a8996f9dca",
+    "url": "/ReactDirectUser/static/css/main.cd1231b9.chunk.css"
+  },
+  {
+    "revision": "4814d707456a70487fd2",
+    "url": "/ReactDirectUser/static/js/2.6f76eca0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactDirectUser/static/js/2.6f76eca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f128e182a8996f9dca",
+    "url": "/ReactDirectUser/static/js/main.ebc249bd.chunk.js"
+  },
+  {
+    "revision": "5889b49a132ff6754ef7",
+    "url": "/ReactDirectUser/static/js/runtime-main.17961703.js"
+  }
+]);
